e4614baa68aa18d4c1d928ad975006ac246035bb
phamduy22041998@gmail.com <phamduy22041998@gmail.com>
2019-11-09 15:39:20 +0700

    xoa bot

--- controllers/index.js
+++ controllers/index.js
@@ -212,15 +212,3 @@ function createEventBtnXoa() {
 }
 
 LoadMenu();
-
-function xoaMenu() {
-  console.log("xoa menu");
-}
-
-//callback function là function đóng vai trò là tham số truyền vào 1 function khác
-//callback function là function đóng vai trò là tham số truyền vào 1 function khá
-
-
-function capNhatMenu() {
-  console.log('asdas');
-}
